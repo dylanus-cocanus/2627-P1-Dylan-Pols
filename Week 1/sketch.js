f936048db78c0f71681217214d03043c9287644f
Dylan <dylan.pols@gmail.com>
2026-09-09 09:57:16 +0200

    Update sketch.js

--- Week 1/sketch.js
+++ Week 1/sketch.js
@@ -1,8 +1,28 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(220);
-  circle(350,300,200)
+  background(150)
+fill("blue")
+rect(0,0,300,200)
+fill("red")
+rect(0,200,300,300)
+fill("white")
+rect(0,450,400,350)
+fill("black")
+rect(300,0,400,250)
+fill("yellow")
+rect(700,0,100,300)
+fill("red")
+rect(400,600,300,200)
+fill("white")
+rect(500,500,150,300)
+fill("red")
+rect(425,300,400,200)
+//fill("green")
+//rect(,,,)
+
+
 }
+  
