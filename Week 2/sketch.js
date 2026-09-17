dfe8b2a1fad0fd064ad16567f211b2184e865449
Dylan <dylan.pols@gmail.com>
2026-09-17 12:43:23 +0200

    Update sketch.js

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,30 +1,48 @@
-let posistieAuto = 100
+let posistieAuto1 = 100
+let posistieAuto2 = 0
 let positieWolk = 250
-let positieWereld = -2000
+let positieWereld = 0
+let postitieBerg = 0
 function setup() {
-  createCanvas(1000, 1500);
+  createCanvas(2000, 1500);
 }
 
 function draw() {
   background(200);
-
+if(positieWereld >= -1500 && positieWereld <=1200){
+  fill("orange")
+}
+if(positieWereld >= -1201 && positieWereld <=6000){
   fill(120,150,200)
+}
+if(positieWereld >=6001 && positieWereld <= 8000){
+  fill('black')
+}
   rect(positieWereld + 0,0,4000,1500)
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
   fill("yellow")
   circle(positieWereld + 150, 100, 150  )
   fill("white")
   circle(positieWereld + 3900, 100, 120)
   strokeWeight(0)
   fill(75)
-  triangle(positieWereld + -220,1000,positieWereld + 460,10,positieWereld + 1500, 1000)
-  triangle(positieWereld + 1500,1250,positieWereld + 2680,250,positieWereld + 3420,1230)
+  triangle(postitieBerg + -220,1000,postitieBerg + 460,10,postitieBerg + 1500, 1000)
+  triangle(postitieBerg + 1500,1250,postitieBerg + 2680,250,postitieBerg + 3420,1230)
   fill(50)
   triangle(positieWereld + 1075,1120,positieWereld + 1730,360,positieWereld + 2300,1200)
   triangle(positieWereld + 2880,1200,positieWereld + 3600,120,positieWereld + 4500,1300)
   fill(250)
-  triangle(positieWereld+ 345,173,positieWereld + 460,10,positieWereld + 640,180)
+  triangle(postitieBerg + 345,173,postitieBerg + 460,10,postitieBerg + 640,180)
   triangle(positieWereld + 3450,344,positieWereld + 3600,120,positieWereld + 3785,360)
-  triangle(positieWereld + 2570,340,positieWereld + 2680,250,positieWereld + 2750,340)
+  triangle(postitieBerg + 2570,340,postitieBerg + 2680,250,postitieBerg + 2750,340)
     fill(75)
   rect(positieWereld + 0,1000,4000,300)
   fill("green")
@@ -50,60 +68,70 @@ function draw() {
   circle(positieWereld + 400,615,350)
   circle(positieWereld + 875,570,350)
 
-  // triangle(1650,1150,2050,1060,2050,1250)
-  // fill("red")
-  // rect(1350,1100,400,100)
-  // rect(1350,1050,300,50)
-  // fill("white")
-  // rect(1650,1050,100,50)
-  // fill("black")
-  // circle(1685,1200,100)
-  // circle(1405,1200,100)
-  // fill("white")
-  // circle(1685,1200,50)
-  // circle(1405,1200,50)
+  fill("yellow")
+  triangle(posistieAuto2 + 1650,1150,2050,1060,2050,1250)
+  fill("blue")
+  rect(posistieAuto2 + 1350,1100,400,100)
+  rect(posistieAuto2 + 1350,1050,300,50)
+  fill("white")
+  rect(posistieAuto2 + 1650,1050,100,50)
+  fill("black")
+  circle(posistieAuto2 + 1685,1200,100)
+  circle(posistieAuto2 + 1405,1200,100)
+  fill("white")
+  circle(posistieAuto2 + 1685,1200,50)
+  circle(posistieAuto2 + 1405,1200,50)
 
 
    fill("yellow")
-  triangle(posistieAuto + 300,1150,posistieAuto + 700,1060,posistieAuto + 700,1250)
+  triangle(positieWereld + posistieAuto + 300,1150,positieWereld + posistieAuto + 700,1060,positieWereld + posistieAuto + 700,1250)
   fill("red")
-  rect(posistieAuto,1100,400,100)
-  rect(posistieAuto,1050,300,50)
+  rect(positieWereld + posistieAuto1,1100,400,100)
+  rect(positieWereld + posistieAuto1,1050,300,50)
   fill("white")
-  rect(posistieAuto + 300,1050,100,50)
+  rect(positieWereld + posistieAuto1 + 300,1050,100,50)
   fill("black")
-  circle(posistieAuto + 335,1200,100)
-  circle(posistieAuto + 55,1200,100)
+  circle(positieWereld + posistieAuto1 + 335,1200,100)
+  circle(positieWereld + posistieAuto1 + 55,1200,100)
   fill("white")
-  circle(posistieAuto + 335,1200,50)
-  circle(posistieAuto + 55,1200,50)
-  posistieAuto = posistieAuto + 15
-  if(posistieAuto >= 4300){
-    posistieAuto = -400
+  circle(positieWereld + posistieAuto1 + 335,1200,50)
+  circle(positieWereld + posistieAuto1 + 55,1200,50)
+  posistieAuto1 = posistieAuto1 + 15
+  if(posistieAuto1 >= 4300){
+    posistieAuto1 = -400
   }
 
   text(posistieAuto,900,50)
 
-  circle(positieWolk,250,200)
-  circle(positieWolk + 165,260,200)
-  circle(positieWolk + 250,250,200)
-  circle(positieWolk + 180,160,200)
-  circle(positieWolk + 85,215,200)  
+  circle(positieWereld + positieWolk,250,200)
+  circle(positieWereld + positieWolk + 165,260,200)
+  circle(positieWereld + positieWolk + 250,250,200)
+  circle(positieWereld + positieWolk + 180,160,200)
+  circle(positieWereld + positieWolk + 85,215,200)  
   positieWolk = positieWolk - 5
   if(positieWolk <= -400){
     positieWolk = 4200
   }
 
-  positieWereld = positieWereld + 3
- if(mouseX >= 2500)
+ if(mouseX >= 1400){
   positieWereld = positieWereld - 3
- else(mouseX <= 1500)
-  text(positieWolk,900,100)
+ }
+ else if(mouseX <= 300){
+  positieWereld = positieWereld + 3
+ }
+if(mouseX >= 1400){
+  postitieBerg = postitieBerg - 2
+ }
+ else if(mouseX <= 300){
+  postitieBerg = postitieBerg + 2,5
+ }
+
+  text(positieWereld,900,100)
 
 
     fill(1)
     textAlign(CENTER);
   textSize(50);
 
-  text(`x: ${int(mouseX)} y: ${int(mouseY)}`, 2000, 50);
+  text(`x: ${int(mouseX)} y: ${int(mouseY)}`, 600, 50);
 }
