960b7057eeb9c4d83624491589d9f352f011ce04
Dylan <dylan.pols@gmail.com>
2026-09-17 11:49:42 +0200

    Update sketch.js

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -1,49 +1,104 @@
+let posistieAuto = 100
+let positieWolk = 250
+let positieWereld = -2000
 function setup() {
-  createCanvas(4000, 1500);
+  createCanvas(1000, 1500);
 }
 
 function draw() {
   background(200);
 
-  background(120,150,200);
+  fill(120,150,200)
+  rect(positieWereld + 0,0,4000,1500)
   fill("yellow")
-  circle(150, 100, 150  )
+  circle(positieWereld + 150, 100, 150  )
   fill("white")
-  circle(3900, 100, 120)
+  circle(positieWereld + 3900, 100, 120)
   strokeWeight(0)
   fill(75)
-  triangle(-220, 1000,460,10, 1500, 1000)
-  triangle(1500,1250,2680,250,3420,1230)
+  triangle(positieWereld + -220,1000,positieWereld + 460,10,positieWereld + 1500, 1000)
+  triangle(positieWereld + 1500,1250,positieWereld + 2680,250,positieWereld + 3420,1230)
   fill(50)
-  triangle(1075,1120,1730,360,2300,1200)
-  triangle(2880,1200,3600,120,4500,1300)
+  triangle(positieWereld + 1075,1120,positieWereld + 1730,360,positieWereld + 2300,1200)
+  triangle(positieWereld + 2880,1200,positieWereld + 3600,120,positieWereld + 4500,1300)
   fill(250)
-  triangle(345,173,460,10,640,180)
-  triangle(3450,344,3600,120,3785,360)
-  triangle(2570,340,2680,250,2750,340)
+  triangle(positieWereld+ 345,173,positieWereld + 460,10,positieWereld + 640,180)
+  triangle(positieWereld + 3450,344,positieWereld + 3600,120,positieWereld + 3785,360)
+  triangle(positieWereld + 2570,340,positieWereld + 2680,250,positieWereld + 2750,340)
     fill(75)
-  rect(0,1000,4000,300)
+  rect(positieWereld + 0,1000,4000,300)
   fill("green")
-  rect(0,1300,4000,200)
-  rect(0,900,4000,100)
+  rect(positieWereld + 0,1300,4000,200)
+  rect(positieWereld + 0,900,4000,100)
   fill("white")
-  rect(0,1130,200,50)
-  rect(300,1130,300,50)
-  rect(700,1130,300,50)
-  rect(1100,1130,300,50)
-  rect(1500,1130,300,50)
-  rect(1900,1130,300,50)
-  rect(2300,1130,300,50)
-  rect(2700,1130,300,50)
-  rect(3100,1130,300,50)
-  rect(3500,1130,300,50)
-  rect(3900,1130,300,50)
+  rect(positieWereld + 0,1130,200,50)
+  rect(positieWereld + 300,1130,300,50)
+  rect(positieWereld + 700,1130,300,50)
+  rect(positieWereld + 1100,1130,300,50)
+  rect(positieWereld + 1500,1130,300,50)
+  rect(positieWereld + 1900,1130,300,50)
+  rect(positieWereld + 2300,1130,300,50)
+  rect(positieWereld + 2700,1130,300,50)
+  rect(positieWereld + 3100,1130,300,50)
+  rect(positieWereld + 3500,1130,300,50)
+  rect(positieWereld + 3900,1130,300,50)
   fill(101,67,33)
-  rect(350,950,100,-300)
-  rect(830,950,100,-300)
+  rect(positieWereld + 350,950,100,-300)
+  rect(positieWereld + 830,950,100,-300)
+
   fill("green")
-  circle(400,615,350)
-  circle(875,570,350)
+  circle(positieWereld + 400,615,350)
+  circle(positieWereld + 875,570,350)
+
+  // triangle(1650,1150,2050,1060,2050,1250)
+  // fill("red")
+  // rect(1350,1100,400,100)
+  // rect(1350,1050,300,50)
+  // fill("white")
+  // rect(1650,1050,100,50)
+  // fill("black")
+  // circle(1685,1200,100)
+  // circle(1405,1200,100)
+  // fill("white")
+  // circle(1685,1200,50)
+  // circle(1405,1200,50)
+
+
+   fill("yellow")
+  triangle(posistieAuto + 300,1150,posistieAuto + 700,1060,posistieAuto + 700,1250)
+  fill("red")
+  rect(posistieAuto,1100,400,100)
+  rect(posistieAuto,1050,300,50)
+  fill("white")
+  rect(posistieAuto + 300,1050,100,50)
+  fill("black")
+  circle(posistieAuto + 335,1200,100)
+  circle(posistieAuto + 55,1200,100)
+  fill("white")
+  circle(posistieAuto + 335,1200,50)
+  circle(posistieAuto + 55,1200,50)
+  posistieAuto = posistieAuto + 15
+  if(posistieAuto >= 4300){
+    posistieAuto = -400
+  }
+
+  text(posistieAuto,900,50)
+
+  circle(positieWolk,250,200)
+  circle(positieWolk + 165,260,200)
+  circle(positieWolk + 250,250,200)
+  circle(positieWolk + 180,160,200)
+  circle(positieWolk + 85,215,200)  
+  positieWolk = positieWolk - 5
+  if(positieWolk <= -400){
+    positieWolk = 4200
+  }
+
+  positieWereld = positieWereld + 3
+ if(mouseX >= 2500)
+  positieWereld = positieWereld - 3
+ else(mouseX <= 1500)
+  text(positieWolk,900,100)
 
 
     fill(1)
