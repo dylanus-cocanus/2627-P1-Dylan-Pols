c596f1f4d6fc293e678dc3553c4da3eb440ad48d
Dylan <dylan.pols@gmail.com>
2026-09-17 14:30:19 +0200

    Update sketch.js

--- Oefenopdrachten/Opdracht 4/sketch.js
+++ Oefenopdrachten/Opdracht 4/sketch.js
@@ -1,3 +1,6 @@
+let B = 0
+let b = 0
+
 function setup() {
   createCanvas(400, 400);
 }
@@ -5,21 +8,6 @@ function setup() {
 
 
 function draw() {
-  background(220);
-
-  background('skyblue');
-
-  // Style the circle using a color gradient.
-    let myGradient = drawingContext.createRadialGradient(50, 50, 3, 50, 50, 40);
-    myGradient.addColorStop(0, 'yellow');
-    myGradient.addColorStop(0.6, 'orangered');
-    myGradient.addColorStop(1, 'yellow');
-    drawingContext.fillStyle = myGradient;
-    drawingContext.strokeStyle = 'rgba(0, 0, 0, 0)';
-
-    // Draw the circle.
-    circle(50, 50, 40);
-
-  describe('A fiery sun drawn on a light blue background.');
+  background(150);
 }
 
