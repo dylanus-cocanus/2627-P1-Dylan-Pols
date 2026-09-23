c32aeb88a3a89c5ef441624111bbc1de7c2de0c7
Dylan <dylan.pols@gmail.com>
2026-09-23 10:37:30 +0200

    Update sketch.js

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,103 +1,96 @@
-let Player = 1
-let RectX1 = 100
-let RectX2 = 255
-let RectX3 = 410
-let RectY1 = 100
-let RectY2 = 255
-let RectY3 = 410
-let RectW = 150
-let RectH = 150
-let Box1 = "white"
-let Box2 = "white"
-let Box3 = "white"
-let Box4 = "white"
-let Box5 = "white"
-let Box6 = "white"
-let Box7 = "white"
-let Box8 = "white"
-let Box9 = "white"
+let vakje1 = "white"
+let vakje2 = "white"
+let vakje3 = "white"
+let vakje4 = "white"
+let vakje5 = "white"
+let vakje6 = "white"
+let vakje7 = "white"
+let vakje8 = "white"
+let vakje9 = "white"
 
-
-
+let celgrote = 100
+let celbuffer = 50
+let celstart = 200
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(800, 800);
 }
 
 function draw() {
-  background(0);
-
-//logic
-
-if (
-  mouseX > RectX1 && mouseX < RectX1 + RectW &&
-  mouseY > RectY1 && mouseY < RectY1 + RectH 
-){
-  Box1 = "grey"
-}else
-  Box1 = "white"
-
-if (
-  mouseX > RectX2 && mouseX < RectX2 + RectW &&
-  mouseY > RectY1 && mouseY < RectY1 + RectH
-){
-  Box2 = "grey"
-}else{
-  Box2 = "white"
-}
-if (
-  mouseX > RectX3 && mouseX < RectX3 + RectW &&
-  mouseY > RectY1 && mouseY < RectY1 + RectH
-){
-  Box3 = "grey"
-}else{
-  Box3 = "white"
-}
-if (
-  mouseX > RectX1 && mouseX < RectX1 + RectW &&
-  mouseY > RectY2 && mouseY < RectY2 + RectH
-){
-  Box4 = "grey"
-}else{
-  Box4 = "white"
-}
-
-
-
-  strokeWeight(0)
-  fill(Box1)
-  rect (RectX1,RectY1, RectW,RectH)
-
-  strokeWeight(0)
-  fill(Box2)
-  rect (RectX2,RectY1, RectW,RectH)
-
-  strokeWeight(0)
-  fill(Box3)
-  rect (RectX3,RectY1, RectW,RectH)
-
-  strokeWeight(0)
-  fill(Box4)
-  rect (RectX1,RectY2, RectW,RectH)
-
-  strokeWeight(0)
-  fill(Box5)
-  rect (RectX2,RectY2, RectW,RectH)
-
-  strokeWeight(0)
-  fill(Box6)
-  rect (RectX3,RectY2, RectW,RectH)
-
-  strokeWeight(0)
-  fill(Box7)
-  rect (RectX1,RectY3, RectW,RectH)
-
-  strokeWeight(0)
-  fill(Box8)
-  rect (RectX2,RectY3, RectW,RectH)
-
-  strokeWeight(0)
-  fill(Box9)
-  rect (RectX3,RectY3, RectW,RectH)
-
+  background(20);
+  let vakjex = celstart;
+  let vakjey = celstart;
+  if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+    strokeWeight(4)
+  } else {
+    strokeWeight(1)
+  }
+  fill(vakje1)
+  rect(vakjex,vakjey,celgrote,celgrote)
+  vakjex += celgrote + celbuffer
+  if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+    strokeWeight(4)
+  } else {
+    strokeWeight(1)
+  }
+  fill(vakje2)
+   rect(vakjex,vakjey,celgrote,celgrote)
+    vakjex += celgrote + celbuffer
+  if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+    strokeWeight(4)
+  } else {
+    strokeWeight(1)
+  }
+  fill(vakje3)
+   rect(vakjex,vakjey,celgrote,celgrote)
+  vakjex = celstart
+  vakjey += celgrote + celbuffer
+  if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+    strokeWeight(4)
+  } else {
+    strokeWeight(1)
+  }
+  fill(vakje4)
+    rect(vakjex,vakjey,celgrote,celgrote)
+  vakjex += celgrote + celbuffer
+  if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+    strokeWeight(4)
+  } else {
+    strokeWeight(1)
+  }
+  fill(vakje5)
+   rect(vakjex,vakjey,celgrote,celgrote)
+    vakjex += celgrote + celbuffer
+  if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+    strokeWeight(4)
+  } else {
+    strokeWeight(1)
+  }
+  fill(vakje6)
+   rect(vakjex,vakjey,celgrote,celgrote)
+     vakjex = celstart
+  vakjey += celgrote + celbuffer
+  if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+    strokeWeight(4)
+  } else {
+    strokeWeight(1)
+  }
+  fill(vakje7)
+    rect(vakjex,vakjey,celgrote,celgrote)
+  vakjex += celgrote + celbuffer
+  if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+    strokeWeight(4)
+  } else {
+    strokeWeight(1)
+  }
+  fill(vakje8)
+   rect(vakjex,vakjey,celgrote,celgrote)
+    vakjex += celgrote + celbuffer
+  if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+    strokeWeight(4)
+  } else {
+    strokeWeight(1)
+  }
+  fill(vakje9)
+   rect(vakjex,vakjey,celgrote,celgrote)
 }
