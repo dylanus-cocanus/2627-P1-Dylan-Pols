42eb43e82e14aa902a2f337d15bbc822e8a71d5a
Dylan <dylan.pols@gmail.com>
2026-09-23 15:04:37 +0200

    Update sketch.js

--- Week 3/sketch.js
+++ Week 3/sketch.js
@@ -1,4 +1,5 @@
-player = 1
+let player = 1
+let kleurvlakje = "white"
 let vakje1 = "white"
 let vakje2 = "white"
 let vakje3 = "white"
@@ -9,6 +10,8 @@ let vakje7 = "white"
 let vakje8 = "white"
 let vakje9 = "white"
 
+let er_word_geklikt = 0
+let binnenvakje = 0
 let celgrote = 100
 let celbuffer = 50
 let celstart = 200
@@ -21,9 +24,17 @@ function setup() {
 
 function draw() {
   background(20);
+  textSize(40)
+  fill(kleurvlakje)
+  text("speler" + player, 330, 100)
   let vakjex = celstart;
   let vakjey = celstart;
   if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+    binnenvakje = 1
+    if(er_word_geklikt == 1 ){
+      vakje1 = kleurvlakje
+      er_word_geklikt = 0
+    }
     strokeWeight(4)
   } else {
     strokeWeight(1)
@@ -32,6 +43,11 @@ function draw() {
   rect(vakjex,vakjey,celgrote,celgrote)
   vakjex += celgrote + celbuffer
   if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+     binnenvakje = 1
+       if(er_word_geklikt == 1 ){
+      vakje2 = kleurvlakje
+      er_word_geklikt = 0
+    }
     strokeWeight(4)
   } else {
     strokeWeight(1)
@@ -40,6 +56,11 @@ function draw() {
    rect(vakjex,vakjey,celgrote,celgrote)
     vakjex += celgrote + celbuffer
   if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+     binnenvakje = 1
+    if(er_word_geklikt == 1 ){
+      vakje3 = kleurvlakje
+      er_word_geklikt = 0
+    }
     strokeWeight(4)
   } else {
     strokeWeight(1)
@@ -49,6 +70,11 @@ function draw() {
   vakjex = celstart
   vakjey += celgrote + celbuffer
   if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+     binnenvakje = 1
+        if(er_word_geklikt == 1 ){
+      vakje4 = kleurvlakje
+      er_word_geklikt = 0
+    }
     strokeWeight(4)
   } else {
     strokeWeight(1)
@@ -57,6 +83,11 @@ function draw() {
     rect(vakjex,vakjey,celgrote,celgrote)
   vakjex += celgrote + celbuffer
   if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+     binnenvakje = 1
+       if(er_word_geklikt == 1 ){
+      vakje5 = kleurvlakje
+      er_word_geklikt = 0
+    }
     strokeWeight(4)
   } else {
     strokeWeight(1)
@@ -65,6 +96,11 @@ function draw() {
    rect(vakjex,vakjey,celgrote,celgrote)
     vakjex += celgrote + celbuffer
   if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+     binnenvakje = 1
+       if(er_word_geklikt == 1 ){
+      vakje6 = kleurvlakje
+      er_word_geklikt = 0
+    }
     strokeWeight(4)
   } else {
     strokeWeight(1)
@@ -74,6 +110,11 @@ function draw() {
      vakjex = celstart
   vakjey += celgrote + celbuffer
   if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+     binnenvakje = 1
+        if(er_word_geklikt == 1 ){
+      vakje7 = kleurvlakje
+      er_word_geklikt = 0
+    }
     strokeWeight(4)
   } else {
     strokeWeight(1)
@@ -82,6 +123,11 @@ function draw() {
     rect(vakjex,vakjey,celgrote,celgrote)
   vakjex += celgrote + celbuffer
   if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+     binnenvakje = 1
+       if(er_word_geklikt == 1 ){
+      vakje8 = kleurvlakje
+      er_word_geklikt = 0
+    }
     strokeWeight(4)
   } else {
     strokeWeight(1)
@@ -90,10 +136,31 @@ function draw() {
    rect(vakjex,vakjey,celgrote,celgrote)
     vakjex += celgrote + celbuffer
   if(mouseX >= vakjex && mouseX <= vakjex + celgrote && mouseY >= vakjey && mouseY <= vakjey + celgrote) {
+     binnenvakje = 1
+        if(er_word_geklikt == 1 ){
+      vakje9 = kleurvlakje
+      er_word_geklikt = 0
+    }
     strokeWeight(4)
   } else {
     strokeWeight(1)
   }
   fill(vakje9)
    rect(vakjex,vakjey,celgrote,celgrote)
+}
+
+function mousePressed() {
+  er_word_geklikt = 1
+  if(binnenvakje == 1){
+  player += 1
+  }
+  if(player >= 3) {
+    player = 1
+  }
+  if(player == 1) {
+    kleurvlakje = "red"
+  }
+  else{
+    kleurvlakje = "blue"
+  }
 }
